b591c156be0a039163f465a2751888b35f2dd893
Nicolas Cecchi <mail>
2023-05-10 16:02:18 +0200

    correct tab-space usage in bayesian_a_l_d_d

--- query_strategies/bayesian_active_learning_disagreement_dropout.py
+++ query_strategies/bayesian_active_learning_disagreement_dropout.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from .strategy import Strategy
+from torch.utils.data import DataLoader
 
 class BALDDropout(Strategy):
 	def __init__(self, X, Y, idxs_lb, idxs_val, net, handler, args, n_drop=10):
@@ -17,15 +18,15 @@ class BALDDropout(Strategy):
 		return idxs_unlabeled[U.sort()[1][:n]]
         
 	def predict_prob_dropout_split(self, X, Y, n_drop):
-        loader_te = DataLoader(self.handler(X, Y, transform=self.args['transformTest']),shuffle=False, **self.args['loader_te_args'])
-
-        self.clf.train()
-        probs = torch.zeros([n_drop, len(Y), len(np.unique(Y))])
-        with torch.no_grad():
-            for i in range(n_drop):
-                print('n_drop {}/{}'.format(i+1, n_drop))
-                for x, y, idxs in loader_te:
-                    x, y = Variable(x.cuda()), Variable(y.cuda())
-                    out, e1 = self.clf(x)
-                    probs[i][idxs] += F.softmax(out, dim=1).cpu().data
-            return probs
+		loader_te = DataLoader(self.handler(X, Y, transform=self.args['transformTest']),shuffle=False, **self.args['loader_te_args'
+		self.clf.train()
+		probs = torch.zeros([n_drop, len(Y), len(np.unique(Y))])
+		with torch.no_grad():
+		    for i in range(n_drop):
+		        print('n_drop {}/{}'.format(i+1, n_drop))
+	        	for x, y, idxs in loader_te:
+		            x, y = Variable(x.cuda()), Variable(y.cuda())
+	            	out, e1 = self.clf(x)
+	            	probs[i][idxs] += F.softmax(out, dim=1).cpu().data
+	    probs /= n_drop
+		return probs
